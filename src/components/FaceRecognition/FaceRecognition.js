f5bccba4373553e17c2a3b84638af7fed38fd7cd
Pooja Ravi <poojaravi20@gmail.com>
2018-10-07 07:51:34 +0400

    adding the front end for face detection

---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './FaceRecognition.css'
+
+const FaceRecognition = ({ imageUrl, boxes }) => {
+	
+	return (
+		<div className='center ma'>
+			<div className='absolute mt2'>
+			<img id='inputImage' alt='' src={imageUrl} width='500px' height='auto' />
+			{	boxes.map((box) =>
+				<div key={`box${box.topRow}${box.rightCol}`} className='bounding-box' style={{top: box.topRow, right: box.rightCol,
+					bottom: box.bottomRow, left: box.leftCol}}/>
+			)
+			}
+				<div>
+					{`Number Of Human Faces Detected: ${boxes.length}`}
+				</div>
+
+			</div>
+		</div>
+	);
+}
+export default FaceRecognition;
